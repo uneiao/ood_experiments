3c8900e36ffcbbd30f095430a3cc5e2aadd927e4
uneiao <uneiao@gmail.com>
2021-05-04 21:16:56 -0400

    change vsc arch

--- src/model/vsc.py
+++ src/model/vsc.py
@@ -18,13 +18,83 @@ class VSC(nn.Module):
         self.register_buffer('prior_slab_mean', torch.zeros(1))
         self.register_buffer('prior_slab_std', torch.ones(1))
 
-        self.fc1 = nn.Linear(784, 400)
-        self.fc21 = nn.Linear(400, self.cfg.vsc.z_dim)
-        self.fc22 = nn.Linear(400, self.cfg.vsc.z_dim)
-        self.fc3 = nn.Linear(self.cfg.vsc.z_dim, 400)
-        self.fc4 = nn.Linear(400, 784)
+        self.enc = nn.Sequential(
+            nn.Conv2d(3, 16, 4, 2, 1),
+            nn.CELU(),
+            nn.GroupNorm(4, 16),
+            nn.Conv2d(16, 64, 4, 2, 1),
+            nn.CELU(),
+            nn.GroupNorm(8, 64),
+            nn.Conv2d(64, 128, 3, 2, 1),
+            nn.CELU(),
+            nn.GroupNorm(16, 128),
+            nn.Conv2d(128, 256, 3, 1, 1),
+            nn.CELU(),
+            nn.GroupNorm(32, 256),
+        )
+        #self.fc1 = nn.Linear(784, 400)
+        self.fc21 = nn.Linear(256, self.cfg.vsc.z_dim)
+        self.fc22 = nn.Linear(256, self.cfg.vsc.z_dim)
+        self.fc23 = nn.Linear(256, self.cfg.vsc.z_dim)
+        #self.fc3 = nn.Linear(self.cfg.vsc.z_dim, 400)
+        #self.fc4 = nn.Linear(400, 784)
 
-        self.fc23 = nn.Linear(400, self.cfg.vsc.z_dim)
+        #self.fc1 = nn.Linear(784, 400)
+        #self.fc21 = nn.Linear(400, self.cfg.vsc.z_dim)
+        #self.fc22 = nn.Linear(400, self.cfg.vsc.z_dim)
+        #self.fc3 = nn.Linear(self.cfg.vsc.z_dim, 400)
+        #self.fc4 = nn.Linear(400, 784)
+
+        #self.fc23 = nn.Linear(400, self.cfg.vsc.z_dim)
+
+
+        self.dec = nn.Sequential(
+            nn.Conv2d(arch.z_what_dim, 256, 1),
+            nn.CELU(),
+            nn.GroupNorm(16, 256),
+
+            nn.Conv2d(256, 128 * 2 * 2, 1),
+            nn.PixelShuffle(2),
+            nn.CELU(),
+            nn.GroupNorm(16, 128),
+            nn.Conv2d(128, 128, 3, 1, 1),
+            nn.CELU(),
+            nn.GroupNorm(16, 128),
+
+            nn.Conv2d(128, 128 * 2 * 2, 1),
+            nn.PixelShuffle(2),
+            nn.CELU(),
+            nn.GroupNorm(16, 128),
+            nn.Conv2d(128, 128, 3, 1, 1),
+            nn.CELU(),
+            nn.GroupNorm(16, 128),
+
+            nn.Conv2d(128, 64 * 2 * 2, 1),
+            nn.PixelShuffle(2),
+            nn.CELU(),
+            nn.GroupNorm(8, 64),
+            nn.Conv2d(64, 64, 2, 1),
+            nn.CELU(),
+            nn.GroupNorm(8, 64),
+
+            nn.Conv2d(64, 32 * 2 * 2, 1),
+            nn.PixelShuffle(2),
+            nn.CELU(),
+            nn.GroupNorm(8, 32),
+            nn.Conv2d(32, 32, 3, 1, 1),
+            nn.CELU(),
+            nn.GroupNorm(8, 32),
+
+            nn.Conv2d(32, 16 * 2 * 2, 1),
+            nn.PixelShuffle(2),
+            nn.CELU(),
+            nn.GroupNorm(4, 16),
+            nn.Conv2d(16, 16, 3, 1, 1),
+            nn.CELU(),
+            nn.GroupNorm(4, 16),
+
+            nn.Conv2d(16, 1, 3, 1, 1),
+        )
 
         self.spike_mode = 'tonolini_original'
         self.register_buffer(
@@ -48,7 +118,8 @@ class VSC(nn.Module):
         return Normal(self.prior_slab_mean, self.prior_slab_std)
 
     def encode(self, x):
-        h1 = F.relu(self.fc1(x))
+        #h1 = F.relu(self.fc1(x))
+        h1 = F.relu(self.enc(x)).view(x.size(0), -1)
 
         location, scale = self.fc21(h1), self.fc22(h1)
         scale = F.softplus(scale)
@@ -69,8 +140,10 @@ class VSC(nn.Module):
         return z, z_slab_posterior, spike_posterior
 
     def decode(self, z):
-        h3 = F.relu(self.fc3(z))
-        return torch.sigmoid(self.fc4(h3))
+        #h3 = F.relu(self.fc3(z))
+        #return torch.sigmoid(self.fc4(h3))
+        x = self.dec(z.view(z.size(0), -1, 1, 1))
+        return torch.sigmoid(x)
 
     def likelihood(self, x, x_p):
         return Normal(x_p, self.cfg.vae.recon_std).log_prob(x)
