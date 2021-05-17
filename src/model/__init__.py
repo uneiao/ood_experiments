79f198dd827b9496e032e1df92dbb9bce31d79aa
uneiao <uneiao@gmail.com>
2021-05-16 20:51:53 -0400

    minor change

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,6 +1,7 @@
 from .vae import LaplaceVAE, NormalVAE
 from .vsc import VSC
 from .mathieu19a_vae import SparseVAE
+from .deform_vae import DeformVAE
 
 __all__ = ['get_model']
 
@@ -14,5 +15,7 @@ def get_model(cfg):
         model = NormalVAE(cfg)
     if cfg.model_name == 'mathieu':
         model = SparseVAE(cfg)
+    if cfg.model_name == 'deform_vae':
+        model = DeformVAE(cfg)
 
     return model
